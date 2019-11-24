38bdb122619345372d17a6c33a9d9a1ed25b641c
lintangwisesa <lintangwisesa@ymail.com>
2019-11-24 15:02:58 +0700

    esp8266 server pot

---
+++ 6_server_POTmonitor.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+MDNSResponder mdns;
+const char* ssid = "Lintang";
+const char* password = "lintang2611";
+ESP8266WebServer server(80);
+int pot;
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500); Serial.print(".");
+  };
+  Serial.println("Connected!");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  
+  server.begin();
+}
+
+void loop(){
+  server.handleClient();
+}
+
+void handle_OnConnect(){
+  pot = analogRead(A0);
+  server.send(200, "text/html", web(pot));
+}
+
+String web(int pot){
+  String web1 = "<h1>Hello World!</h1><b>Nilai potensio: " + String(pot) + "</b>";
+  return web1;
+}
+
